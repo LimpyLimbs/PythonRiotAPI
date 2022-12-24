862cb65e64267bbd81c47a922535c734da878875
eddie <eddie.eby@gmail.com>
2022-12-24 04:10:43 -0700

    developing match history

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import requests
 import summoner
 
 response_code = 404
-api_key = {"X-Riot-Token": "RGAPI-1fb5f97f-7355-4d46-b7e0-779ec346093b"}
+api_key = {"X-Riot-Token": "RGAPI-f1ffcb00-eb37-43f1-9697-15ea98c2e319"}
 
+# checks for valid summoner name
 while response_code != 200:
     summoner_name = input("Enter Summoner Name: ")
     response = requests.get(f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}',
                             headers=api_key)
+
     response_code = response.status_code
 
     if response_code != 200:
         print('Summoner name not found. Please try again.')
 
-print(response.json())
+# formats api data to json
+summoner_data = response.json()
+
+summoner_object = summoner.Summoner(summoner_data["id"], summoner_data["accountId"], summoner_data["puuid"], summoner_data["name"], summoner_data["summonerLevel"])
+
+print(summoner_object.summoner_level)
+print(summoner_object.get_match_history())
+
--- summoner.py
+++ summoner.py
@@ -1,5 +1,7 @@
-class Summoner:
+import requests
 
+
+class Summoner:
     def __init__(self, id, account_id, puuid, name, summoner_level):
         self.id = id
         self.account_id = account_id
@@ -7,4 +9,7 @@ class Summoner:
         self.name = name
         self.summoner_level = summoner_level
 
-
+    def get_match_history(self):
+        response = requests.get(f"https://na1.api.riotgames.com/lol/match/v5/matches/by-puuid/{self.puuid}/ids",
+                                headers={"X-Riot-Token": "RGAPI-f1ffcb00-eb37-43f1-9697-15ea98c2e319"})
+        print(self.puuid)
